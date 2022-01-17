d580e672b89ed03a2dacc1a092cacadd7f6c3d9f
phuruytho28 <cufau13@gmail.com>
2022-01-17 18:41:51 +0700

    bai2

---
+++ bai2/src/App1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {   
+             
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Nhap ho va ten: ");
+        String hoVaTen = sc.nextLine();
+
+        System.out.println("Thong bao: xin chao "+ hoVaTen);
+
+    }
+}
